fed151bfdf6470e1f14697947ded7f6b8f42c0c2
AddamGlitch <addamkoroma@gmail.com.com>
2023-01-24 10:08:32 -0700

    Week four First class

--- tests/loginapi.test.js
+++ tests/loginapi.test.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-it ("Should get a login token", async () => {//async alows us to read from the database by moving on and comoing back
+it ("Should get a login token", async () => {//async allows us to read from the database by moving on and comoing back
 
     const  loginResponse = await fetch('https://dev.stedi.me/login', { //await fetch allows it to move on while getting the data 
 
